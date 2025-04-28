9e33bde2fb28490f99fba04fc43d43ecf4495838
Aidar Minyazev <54939061+legodark-hub@users.noreply.github.com>
2025-04-28 14:17:10 +0500

    Revert "Comment out the on_startup function and its usage in the Litestar app configuration"
    This reverts commit 646a6ba0af2996234c5e70688df529c34a12172a.

--- src/app.py
+++ src/app.py
@@ -8,14 +8,14 @@ from src.database.session import sqlalchemy_config
 from src.models.base import BaseModel
 
 
-# async def on_startup() -> None:
-#     async with sqlalchemy_config.get_engine().begin() as conn:
-#         await conn.run_sync(BaseModel.metadata.create_all)
+async def on_startup() -> None:
+    async with sqlalchemy_config.get_engine().begin() as conn:
+        await conn.run_sync(BaseModel.metadata.create_all)
 
 
 app = Litestar(
     route_handlers=[UserController],
-    # on_startup=[on_startup],
+    on_startup=[on_startup],
     plugins=[
         SQLAlchemyInitPlugin(config=sqlalchemy_config),
         SQLAlchemySerializationPlugin(),
